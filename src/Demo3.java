7676d9c69a5bd6a4c67364369580c34dd883b97b
feyzeddin dönmez <feyzeddin71@gmail.com>
2021-06-16 00:11:47 -0400

    demo3

---
+++ src/Demo3.java
@@ -0,0 +1,17 @@
+public class Demo3 {
+
+    public static void main(String[] args) {
+
+
+        String name="Feyzeddin";
+
+        //reverse this string
+
+        System.out.println (10/2);
+
+    }
+
+
+
+
+}
